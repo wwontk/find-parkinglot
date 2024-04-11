9290d84965d1960a56808a2e03db1de1cd1d27e8
wwontk <wwontk@gmail.com>
2024-04-11 19:31:49 +0900

    [Feat] 주차장 디테일 페이지

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,19 @@ import "./App.css";
 import HomePage from "./pages/HomePage";
 import InnerCon from "./components/common/InnerCon";
 import MarketPage from "./pages/MarketPage";
+import ParkingLotDetailPage from "./pages/ParkingLotDetailPage";
 
 function App() {
   return (
     <>
       <InnerCon>
         <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="/market/:marketname" element={<MarketPage />}></Route>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/market/:marketname" element={<MarketPage />} />
+          <Route
+            path="/parkinglot/:prkplceNo"
+            element={<ParkingLotDetailPage />}
+          />
         </Routes>
       </InnerCon>
     </>
---
+++ src/pages/ParkingLotDetailPage/index.tsx
@@ -0,0 +1,13 @@
+import Nav from "../../components/common/Nav";
+import TopTitle from "../../components/common/TopTitle";
+
+const ParkingLotDetailPage = () => {
+  return (
+    <>
+      <TopTitle text="신사역" bg="bg-theme-color" />
+      <Nav />
+    </>
+  );
+};
+
+export default ParkingLotDetailPage;
