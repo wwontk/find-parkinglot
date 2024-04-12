d29b66b06564482029d9880ec3768ebb36c0a0fb
wwontk <wwontk@gmail.com>
2024-04-12 22:45:02 +0900

    [Refactor] 코드 정리

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,31 +1,30 @@
-import { Link } from "react-router-dom";
 import Nav from "../../components/common/Nav";
 import regionList from "../../data/RegionList";
+import MarketList from "../../components/Market/MarketList";
 
 const HomePage = () => {
   return (
     <>
       <div className="mt-12 font-bold text-3xl">
+        <p className="font-medium text-sm">시장가자! 근처에 있는</p>
         <p>주차장</p>
         <p>찾아줘🚘</p>
       </div>
       <div>
         <ul className="my-10 flex whitespace-nowrap overflow-auto">
-          {regionList.map((item) => (
-            <li className="bg-slate-100 px-5 py-2.5 mr-4 rounded-full">
+          {regionList.map((item, index) => (
+            <li
+              key={index}
+              className="bg-slate-100 px-5 py-2.5 mr-4 rounded-full"
+            >
               {item}
             </li>
           ))}
         </ul>
       </div>
       <div className="font-medium mb-7">Traditional Market 🥬</div>
-      <div>
-        <Link to={`/market/강남시장`}>
-          <div className="bg-slate-200 p-5 rounded-2xl">
-            <p className="font-semibold">강남시장</p>
-            <p>서울특별시 강남구 압구정로 2길 46</p>
-          </div>
-        </Link>
+      <div className="mb-28">
+        <MarketList />
       </div>
       <Nav />
     </>
