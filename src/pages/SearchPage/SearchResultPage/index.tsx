32adb66a65ff617a165a14e5ca07828013c9655c
wwontk <wwontk@gmail.com>
2024-04-15 15:39:14 +0900

    [Feat] 검색 페이지 리뉴얼 + 시장 정보 존재 하지 않을 시 오류 처리

--- src/pages/SearchPage/SearchResultPage/index.tsx
+++ src/pages/SearchPage/SearchResultPage/index.tsx
@@ -2,20 +2,38 @@ import { useSearchParams } from "react-router-dom";
 import useGetMarketSearchQuery from "../../../hooks/query/useGetMarketSearchQuery";
 import { marketListItemProps } from "../../../types/MarketList";
 import MarketListItem from "../../../components/Market/MarketListItem";
+import { RiErrorWarningFill } from "react-icons/ri";
 
 const SearchResultPage = () => {
   const [searchParams] = useSearchParams();
   const object = searchParams.get("object");
   const { marketSearchData } = useGetMarketSearchQuery(object);
 
-  console.log(marketSearchData);
+  // console.log(marketSearchData);
   return (
     <>
       <div className="mt-4">
-        {marketSearchData?.response.body.items.map(
-          (item: marketListItemProps) => (
-            <MarketListItem mrktNm={item.mrktNm} rdnmadr={item.rdnmadr} />
+        {marketSearchData?.response.header.resultCode === "00" ? (
+          marketSearchData?.response.body.items.map(
+            (item: marketListItemProps) => (
+              <MarketListItem
+                key={`${item.mrktNm} + ${item.rdnmadr}`}
+                mrktNm={item.mrktNm}
+                rdnmadr={item.rdnmadr}
+              />
+            )
           )
+        ) : (
+          <div className="border p-5 rounded-2xl mb-4">
+            <p className="flex justify-center">
+              <RiErrorWarningFill size={40} color="#e6e6e6" />
+            </p>
+            <p className="text-center mt-4">시장 정보가 존재하지 않습니다😭</p>
+            <div className="text-center text-sm mt-4 text-slate-300">
+              <p>시장의 풀네임을 입력해주세요</p>
+              <p>ex. 구매탄 (x) 구매탄시장 (o)</p>
+            </div>
+          </div>
         )}
       </div>
     </>
--- src/pages/SearchPage/index.tsx
+++ src/pages/SearchPage/index.tsx
@@ -11,35 +11,36 @@ const SearchPage = () => {
     setKeyword(e.target.value);
   };
 
-  const handleOnClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     navigate(`/search/list?object=${keyword}`);
   };
 
   return (
     <>
-      <label className="input input-bordered flex items-center gap-2 mt-1">
+      <form onSubmit={handleSubmit} className="mt-2 rounded border flex p-2">
         <input
           type="text"
-          className="grow"
-          placeholder="Search"
+          className="flex-1"
+          placeholder="시장을 검색해보세요."
           onChange={handleOnChange}
         />
-        <button onClick={handleOnClick}>
+        <button>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 16 16"
             fill="currentColor"
-            className="w-4 h-4 opacity-70"
+            className="w-5 h-5 opacity-70 ml-2"
           >
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-              clip-rule="evenodd"
+              clipRule="evenodd"
             />
           </svg>
         </button>
-      </label>
+      </form>
+
       <Outlet />
       <Nav />
     </>
