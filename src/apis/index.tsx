5b5f4864c60d3596cb40967f89953eb1052c9604
wwontk <wwontk@gmail.com>
2024-04-20 00:13:58 +0900

    [Refactor] api 주소 수정

--- src/apis/index.tsx
+++ src/apis/index.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosDefault = axios.create({
-  baseURL: "http://api.data.go.kr",
+  baseURL: "https://api.data.go.kr",
   headers: {
     "Content-Type": "application/json",
   },
