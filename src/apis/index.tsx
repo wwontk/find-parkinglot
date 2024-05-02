4026c7e87234d39df18fd8f656952c2bc8da779f
wwontk <wwontk@gmail.com>
2024-05-02 17:47:05 +0900

    [Fix] api 주소 수정

--- src/apis/index.tsx
+++ src/apis/index.tsx
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 export const axiosDefault = axios.create({
-  baseURL: "http://api.data.go.kr",
+  baseURL: "https://proxy.cors.sh/http://api.data.go.kr",
   headers: {
     "Content-Type": "application/json",
+    "x-cors-api-key":
+      "live_7e11ac0c11f6b0a7d4f3d11ded385c4fdf4382b926264954727f1200ab482cc8",
   },
 });
