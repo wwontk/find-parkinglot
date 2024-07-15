4d413d6ddfa0dccda7798f7ee240f004189a8da4
wwontk <wwontk@gmail.com>
2024-07-15 14:55:56 +0900

    [feat] 주차장 목업데이터

--- src/components/ParkingLot/ParkingLotList/index.tsx
+++ src/components/ParkingLot/ParkingLotList/index.tsx
@@ -1,35 +1,37 @@
-import { useParams } from "react-router-dom";
-import useGetParkingLotQuery from "../../../hooks/query/useGetParkingLotQuery";
+// import { useParams } from "react-router-dom";
+// import useGetParkingLotQuery from "../../../hooks/query/useGetParkingLotQuery";
 import { parkingLotDataType } from "../../../types/ParkingLot";
 import ParkingLotListItem from "../ParkingLotListItem";
-import { RiErrorWarningFill } from "react-icons/ri";
-import useInfiniteScroll from "../../../hooks/useInfiniteScroll";
-import Skeleton from "../../common/Skeleton";
+// import { RiErrorWarningFill } from "react-icons/ri";
+// import useInfiniteScroll from "../../../hooks/useInfiniteScroll";
+// import Skeleton from "../../common/Skeleton";
+
+import parkingLotData from "../../../data/parkingLotData";
 
 const ParkingLotList = () => {
-  const { marketname } = useParams();
-  const {
-    parkingLotData,
-    parkingLotFecthNextPage,
-    parkingLotHasNextPage,
-    parkingLotIsLoading,
-  } = useGetParkingLotQuery(marketname);
+  // const { marketname } = useParams();
+  // const {
+  //   parkingLotData,
+  //   parkingLotFecthNextPage,
+  //   parkingLotHasNextPage,
+  //   parkingLotIsLoading,
+  // } = useGetParkingLotQuery(marketname);
 
-  const { bottomDiv } = useInfiniteScroll(
-    parkingLotFecthNextPage,
-    parkingLotHasNextPage
-  );
+  // const { bottomDiv } = useInfiniteScroll(
+  //   parkingLotFecthNextPage,
+  //   parkingLotHasNextPage
+  // );
 
-  if (parkingLotIsLoading)
-    return (
-      <>
-        <Skeleton cnt={10} />
-      </>
-    );
+  // if (parkingLotIsLoading)
+  //   return (
+  //     <>
+  //       <Skeleton cnt={5} />
+  //     </>
+  //   );
 
   return (
     <>
-      {parkingLotData?.pages.map((page) =>
+      {/* {parkingLotData?.pages.map((page) =>
         page.response.body.items ? (
           page.response.body.items.item.map((item: parkingLotDataType) => (
             <ParkingLotListItem
@@ -56,8 +58,25 @@ const ParkingLotList = () => {
             <p className="text-center mt-4">근처 공영주차장이 없습니다😭</p>
           </div>
         )
-      )}
-      {bottomDiv()}
+      )} */}
+      {parkingLotData.map((item: parkingLotDataType) => (
+        <ParkingLotListItem
+          key={`${item.prkplceNm}` + `${item.prkplceCnt}`}
+          prkplceNm={item.prkplceNm}
+          prkplceRoadNmAddr={
+            item.prkplceRoadNmAddr === " "
+              ? item.prkplceLotnoAddr
+              : item.prkplceRoadNmAddr
+          }
+          prkplceNo={item.prkplceNo}
+          prkplceLatPos={item.prkplceLatPos}
+          prkplceLotPos={item.prkplceLotPos}
+          prkplceOperInfo={item.prkplceOperInfo}
+          prkplceChargeInfo={item.prkplceChargeInfo}
+          prkplceCnt={item.prkplceCnt}
+        />
+      ))}
+      {/* {bottomDiv()} */}
     </>
   );
 };
